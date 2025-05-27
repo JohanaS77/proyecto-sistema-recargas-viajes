21a38d9f8ef6b566d4d8764a9204a9f50525760d
Johana Saavedra <johana.saavedra77@gmail.com>
2025-05-26 19:22:20 -0500

    actualización

---
+++ sistema-recargas-viajes-proyecto/scripts/04_incidencias.sql
@@ -0,0 +1,12 @@
+INSERT INTO incidencias (viaje_id, descripcion, gravedad)
+VALUES
+(1, 'Falla en el torniquete', 'alta'),
+(2, 'Validación tardía', 'media'),
+(3, 'Pasajero sin tarjeta', 'leve'),
+(4, 'Error en la tarjeta', 'media'),
+(5, 'Desincronización del sensor', 'alta'),
+(6, 'Sin respuesta del dispositivo', 'alta'),
+(7, 'Usuario agredió al conductor', 'alta'),
+(8, 'Recarga no reflejada', 'media'),
+(9, 'Sensor duplicó el viaje', 'media'),
+(10, 'Viaje sin registro en sistema', 'alta');
