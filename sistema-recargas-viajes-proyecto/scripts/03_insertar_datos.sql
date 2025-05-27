bce7a4afc119993d0aa3466596737130ef8f1568
Johana Saavedra <johana.saavedra77@gmail.com>
2025-05-26 19:23:05 -0500

    Update 03_insertar_datos.sql

--- sistema-recargas-viajes-proyecto/scripts/03_insertar_datos.sql
+++ sistema-recargas-viajes-proyecto/scripts/03_insertar_datos.sql
@@ -1,3 +1,4 @@
+-- Inserción de 40 recargas asociadas a promociones
 DO $$
 BEGIN
   FOR i IN 1..40 LOOP
@@ -6,6 +7,22 @@ BEGIN
       i, 
       (5000 + i * 100), 
       CURRENT_DATE - (i % 15), 
+      (1 + (i % 10))
+    );
+  END LOOP;
+END;
+$$;
+
+-- Inserción de 30 viajes con dispositivo (tipo móvil y torniquete)
+DO $$
+BEGIN
+  FOR i IN 1..30 LOOP
+    INSERT INTO viajes (estacion_abordaje_id, fecha, tarifa_id, tarjeta_id, dispositivo_id)
+    VALUES (
+      (1 + (i % 5)), 
+      DATE '2025-04-01' + (i % 30),
+      1, 
+      i, 
       (1 + (i % 10))
     );
   END LOOP;
