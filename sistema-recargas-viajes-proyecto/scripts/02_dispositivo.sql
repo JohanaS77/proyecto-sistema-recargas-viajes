70c3955773fc35e9951d485a8bc7817b95deb54c
Johana Saavedra <johana.saavedra77@gmail.com>
2025-05-26 19:19:52 -0500

    actualización

---
+++ sistema-recargas-viajes-proyecto/scripts/02_dispositivo.sql
@@ -0,0 +1,11 @@
+INSERT INTO dispositivos (tipo, ubicacion) VALUES
+('torniquete', 'Estación Central'),
+('torniquete', 'Estación Norte'),
+('móvil', 'Bus Línea 5'),
+('móvil', 'Bus Línea 7'),
+('sensor', 'Ciclovía 1'),
+('sensor', 'Ciclovía 3'),
+('móvil', 'Bus Exprés 10'),
+('móvil', 'Bus Exprés 20'),
+('torniquete', 'Portal Sur'),
+('sensor', 'Troncal Verde');
